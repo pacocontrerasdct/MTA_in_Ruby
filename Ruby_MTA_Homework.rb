73ebdeca66e0ae7140c77a514e45f0a6fb009214
Paco Contreras <pacocontrerasdct@gmail.com>
2015-08-28 06:00:19 +0100

    Initial commit

---
+++ Ruby_MTA_Homework.rb
@@ -0,0 +1,8 @@
+MTA = {
+  :lineN => ['Time Square','34th','28thN','23rdN','Union Square','8thN'],
+  :lineL => ['8thL','6th','Union Square','3rd','1st'],
+  :line6 => ['Grand Central','33rd','28thVI','23rdVI','Union Square','Astor Place']
+ }
+
+ start = MTA[:lineN][0]
+ puts start
